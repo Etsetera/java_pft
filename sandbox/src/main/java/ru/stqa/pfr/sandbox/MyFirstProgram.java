313ac04ead1166b04e65b314b42f923802fab3e6
Etsetera <etsetera@yandex.ru>
2019-05-08 16:29:44 +0400

    Создание классов + ДЗ Point

--- sandbox/src/main/java/ru/stqa/pfr/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pfr/sandbox/MyFirstProgram.java
@@ -7,24 +7,19 @@ public class MyFirstProgram {
     hello("user");
     hello("Elena");
 
-    double l = 5;
-    System.out.println("Пплощадь квадрата со стороной " + l + "=" + area(l));
+    Square s = new Square(5);
+    System.out.println("Пплощадь квадрата со стороной " + s.l + "=" + s.area());
 
-    double a = 4;
-    double b = 6;
-    System.out.println("Пплощадь прямоугольника со сторонами " + a + " и " + b + "=" + area(a, b));
+    Rectangle r = new Rectangle(4, 6);
+    System.out.println("Пплощадь прямоугольника со сторонами " + r.a + " и " + r.b + "=" + r.area());
+
+    Point p1 = new Point(5, 9);
+    Point p2 = new Point(4, 1);
+    System.out.println("Расстояние между точкой p1 c координатами (" + p1.x + ";" + p1.y + ") и точкой p2 с координатами (" + p2.x + ";" + p2.y + ")" + " = " + p1.distance(p2));
   }
 
   public static void hello(String somebody) {
     System.out.println("Hello, " + somebody + "!");
-  }
-
-  public static double area(double len) {
-    return len * len;
-  }
-
-  public static double area(double a, double b) {
-    return a * b;
   }
 
 }
