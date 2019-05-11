ba4b3dfbde9a56458e3b807a57dcb296b6e2070b
Etsetera <etsetera@yandex.ru>
2019-05-11 19:44:59 +0400

    Реализ. проверка и обеспечение выполнения предусловий (удаление контакта из редактирования)
     удаление контакта из редактирования

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactDeletionFromEditTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactDeletionFromEditTests.java
@@ -1,11 +1,16 @@
 package ru.stqa.pft.addressbook.tests;
 
 import org.testng.annotations.Test;
+import ru.stqa.pft.addressbook.model.ContactData;
 
 public class ContactDeletionFromEditTests extends TestBase {
 
   @Test
-  public void testContractDeletionFromEdit (){
+  public void testContractDeletionFromEdit() {
+    if (!app.getContactHelper().isThereAContact()) {
+      app.getContactHelper().createContact(new ContactData("Elena", "Nikonova", "Samara, st Central 4-67", "89654206522", "nikon@testov.com", "test1"));
+      app.getNavigationHelper().gotoHomePage();
+    }
     app.getContactHelper().initContactModification();
     app.getContactHelper().deleteContactFromEdit();
     app.getNavigationHelper().gotoHomePage();
