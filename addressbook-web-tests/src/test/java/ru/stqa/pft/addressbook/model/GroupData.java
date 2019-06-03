3c2080268e2c3639ab28dd026e27f6407bed5017
Etsetera <etsetera@yandex.ru>
2019-06-03 12:43:22 +0400

    5.4. Fluent-интерфейсы Группы

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/GroupData.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/GroupData.java
@@ -2,30 +2,11 @@ package ru.stqa.pft.addressbook.model;
 
 public class GroupData {
 
-  private int id;
-  private final String name;
+  private int id = Integer.MAX_VALUE;
+  private  String name;
   private  String header;
   private  String footer;
 
-
-  public GroupData(String name, String header, String footer) {
-    this.id = Integer.MAX_VALUE;
-    this.name = name;
-    this.header = header;
-    this.footer = footer;
-  }
-
-  public GroupData(int id, String name, String header, String footer) {
-    this.id = id;
-    this.name = name;
-    this.header = header;
-    this.footer = footer;
-  }
-
-  public GroupData(int id, String name) {
-    this.id = id;
-    this.name = name;
-  }
 
   @Override
   public String toString() {
@@ -50,8 +31,25 @@ public class GroupData {
   public String getFooter() {
     return footer;
   }
-  public void setId(int id) {
+
+  public GroupData withId(int id) {
     this.id = id;
+    return this;
+  }
+
+  public GroupData withName(String name) {
+    this.name = name;
+    return this;
+  }
+
+  public GroupData withHeader(String header) {
+    this.header = header;
+    return this;
+  }
+
+  public GroupData withFooter(String footer) {
+    this.footer = footer;
+    return this;
   }
 
   @Override
