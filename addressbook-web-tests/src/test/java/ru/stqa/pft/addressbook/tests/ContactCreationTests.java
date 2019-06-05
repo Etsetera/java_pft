86d82544235ed6986244d20deabf719813568462
Etsetera <etsetera@yandex.ru>
2019-06-05 14:26:49 +0400

    6.1. Пути к файлам и директориям
    реализовано добавление изображения при создании Контакта

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
@@ -4,6 +4,8 @@ import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
 import ru.stqa.pft.addressbook.model.Contacts;
 
+import java.io.File;
+
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
@@ -12,18 +14,27 @@ public class ContactCreationTests extends TestBase {
   @Test
   public void testContactCreation() throws Exception {
     Contacts before = app.contact().all();
+    File photo = new File("src/test/resources/cat.jpg");
     ContactData contact = new ContactData()
             .withFirstname("Elena").withLastname("Nikonova").withAddress("Samara, st Central 4-67")
             .withHomePhone("+7(111)856").withMobilePhone("22-22").withWorkPhone("33 33 33")
             .withEmail("nikon@testov.com")
             .withEmail2("nikon22@testov.com")
             .withEmail3("nikon33@testov.com")
-            .withGroup("test1");
+            .withGroup("test1").withPhoto(photo);
     app.contact().create(contact);
-    assertThat(app.contact().count(), equalTo(before.size() +1));
+    assertThat(app.contact().count(), equalTo(before.size() + 1));
     Contacts after = app.contact().all();
     assertThat(after, equalTo(
             before.withAdded(contact.withId(after.stream().mapToInt((g) -> g.getId()).max().getAsInt()))));
   }
 
+  @Test(enabled = false)
+  public void testCurrentDir() {
+    File currentDir = new File(".");
+    System.out.println(currentDir.getAbsolutePath());
+    File photo = new File("src/test/resources/cat.jpg");
+    System.out.println(photo.getAbsolutePath());
+    System.out.println(photo.exists());
+  }
 }
