a80ed7762bbe76ee7e9c8d55b316d3662c7fe7b8
Etsetera <etsetera@yandex.ru>
2019-06-05 21:29:22 +0400

    ДЗ_13 Параметризовать тесты для создания контактов
    Реализован для теста создания контактов: провайдер тестовых данных, который читает данные из файла в формате XML

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
@@ -1,27 +1,43 @@
 package ru.stqa.pft.addressbook.tests;
 
+import com.thoughtworks.xstream.XStream;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
 import ru.stqa.pft.addressbook.model.Contacts;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Iterator;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class ContactCreationTests extends TestBase {
 
-  @Test
-  public void testContactCreation() throws Exception {
+
+  @DataProvider
+  public Iterator<Object[]> validContacts() throws IOException {
+    BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/contacts.xml")));
+    String xml = "";
+    String line = reader.readLine();
+    while (line != null) {
+      xml += line;
+      line = reader.readLine();
+    }
+    XStream xstream = new XStream();
+    xstream.processAnnotations(ContactData.class);
+    List<ContactData> contacts = (List<ContactData>) xstream.fromXML(xml);
+    return contacts.stream().map((g) -> new Object[]{g}).collect(Collectors.toList()).iterator();
+  }
+
+  @Test (dataProvider = "validContacts")
+  public void testContactCreation(ContactData contact) throws Exception {
     Contacts before = app.contact().all();
-    File photo = new File("src/test/resources/cat.jpg");
-    ContactData contact = new ContactData()
-            .withFirstname("Elena").withLastname("Nikonova").withAddress("Samara, st Central 4-67")
-            .withHomePhone("+7(111)856").withMobilePhone("22-22").withWorkPhone("33 33 33")
-            .withEmail("nikon@testov.com")
-            .withEmail2("nikon22@testov.com")
-            .withEmail3("nikon33@testov.com")
-            .withGroup("test1").withPhoto(photo);
     app.contact().create(contact);
     assertThat(app.contact().count(), equalTo(before.size() + 1));
     Contacts after = app.contact().all();
