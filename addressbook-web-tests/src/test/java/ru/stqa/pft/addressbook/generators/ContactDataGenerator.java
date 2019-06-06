fc6007c84e3bdcc0d21ebf18b788bd200423b0fd
Etsetera <etsetera@yandex.ru>
2019-06-06 11:35:10 +0400

    6.8. Автоматическое закрытие файлов после использования
    try в writer и reader

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/generators/ContactDataGenerator.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/generators/ContactDataGenerator.java
@@ -52,18 +52,19 @@ public class ContactDataGenerator {
     XStream xstream = new XStream();
     xstream.processAnnotations(ContactData.class);
     String xml = xstream.toXML(contacts);
-    Writer writer = new FileWriter(file);
-    writer.write(xml);
-    writer.close();
+     try (Writer writer = new FileWriter(file)) {
+       writer.write(xml);
+     }
   }
 
   private void saveAsCsv(List<ContactData> contacts, File file) throws IOException {
-    Writer writer = new FileWriter(file);
-    for (ContactData contact : contacts) {
-      writer.write(String.format("%s;%s;%s;%s;%s;%s;%s;%s;%s;%s\n", contact.getFirstname(),contact.getLastname(), contact.getAddress(),contact.getHomePhone(),contact.getMobilePhone()
-              ,contact.getWorkPhone(), contact.getEmail(),contact.getEmail2(), contact.getEmail3(),contact.getGroup()));
-    }
-    writer.close();
+   try (Writer writer = new FileWriter(file)) {
+     for (ContactData contact : contacts) {
+       writer.write(String.format("%s;%s;%s;%s;%s;%s;%s;%s;%s;%s\n", contact.getFirstname(), contact.getLastname()
+               ,contact.getAddress(), contact.getHomePhone(), contact.getMobilePhone()
+               ,contact.getWorkPhone(), contact.getEmail(), contact.getEmail2(), contact.getEmail3(), contact.getGroup()));
+     }
+   }
   }
 
   private List<ContactData> generateContacts(int count) {
