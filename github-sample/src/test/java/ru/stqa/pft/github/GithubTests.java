284101a0bbd4cd2876fd0607510d29662bd3f91a
Etsetera <etsetera@yandex.ru>
2019-06-16 20:35:19 +0400

    9.6. REST: взаимодействие с GitHub

---
+++ github-sample/src/test/java/ru/stqa/pft/github/GithubTests.java
@@ -0,0 +1,19 @@
+package ru.stqa.pft.github;
+
+import com.google.common.collect.ImmutableMap;
+import com.jcabi.github.*;
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+
+public class GithubTests {
+
+  @Test
+  public void testCommits() throws IOException {
+    Github github = new RtGithub("1f7ba9a34f322be07f1340752115e4b16b7e696d");
+    RepoCommits commits = github.repos().get(new Coordinates.Simple("Etsetera", "java_pft")).commits();
+    for (RepoCommit commit : commits.iterate(new ImmutableMap.Builder<String, String>().build())) {
+      System.out.println(new RepoCommit.Smart(commit).message());
+    }
+  }
+}
